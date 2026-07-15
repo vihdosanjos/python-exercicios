297af046e012211269d9cfd02738e159767d9e49
Tory <w966kw9kph@privaterelay.appleid.com>
2026-07-15 17:33:34 -0400

    Rename ex014.py to ex014_conversor_fahrenheit_celsius.py

--- ex000/ex014.py
+++
@@ -1,2 +0,0 @@
-c = float(input('Informe a temperatura em ᴼC: '))
-print('A temperatura de {:.1f}ᴼC corresponde a {:.1f}ᴼF'.format(c, (c*1.8) +32))
---
+++ ex000/ex014_conversor_fahrenheit_celsius.py
@@ -0,0 +1,2 @@
+c = float(input('Informe a temperatura em ᴼC: '))
+print('A temperatura de {:.1f}ᴼC corresponde a {:.1f}ᴼF'.format(c, (c*1.8) +32))
